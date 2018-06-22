ca3f1168a6a411fee99c7cfb2740595f8ae158cb
sandhiyamalar <40415790+sandhiyamalar@users.noreply.github.com>
2018-06-22 14:03:20 +0530

    Create G7.java

---
+++ G7.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class G7{
+    public static void main(String args[]){
+        Scanner in=new Scanner(System.in);
+        String s="Hello";
+        int n=in.nextInt();
+        for(int i=1;i<=n;i++){
+            System.out.println(s);
+        }
+    }
+}
