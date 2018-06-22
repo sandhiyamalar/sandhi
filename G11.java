d8a7cf6d83f85ef942886edee21d25f7ddcfc8e6
sandhiyamalar <40415790+sandhiyamalar@users.noreply.github.com>
2018-06-22 14:45:50 +0530

    Create G11.java

---
+++ G11.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+class G11{
+    public static void main(String args[]){
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int b=in.nextInt();
+        int c=(int)Math.pow(n,b);
+        System.out.println(c);
+    }
+}
