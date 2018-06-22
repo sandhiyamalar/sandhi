656726da716c121da8780d55c03ffe1268064d96
sandhiyamalar <40415790+sandhiyamalar@users.noreply.github.com>
2018-06-22 14:12:46 +0530

    Create G8.java

---
+++ G8.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+class G8{
+    public static void main(String args[]){
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        System.out.println(((n*n+n)/2));
+    }
+}
